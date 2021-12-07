f4d81fddeb889a670ce4f38d7fbb52c6a14389c1
Juan Martinez <jmartinez@benchsci.com>
2021-12-06 22:17:56 -0500

    Day 1 - Add solution to part 2

--- day1/day1-solution1.py
+++ day1/day1-solution1.py
@@ -4,7 +4,7 @@ def solution1():
     currNum = 0
     counter = 0
 
-    with open('day1/day1-input.txt', 'r')as f:
+    with open("./day1/day1-input.txt", "r") as f:
 
         for line in f:
             currNum = int(line)
@@ -14,11 +14,31 @@ def solution1():
 
             prevNum = currNum
 
-    print(f"Solution 1 counter = {counter}")
+    print(f"Day 1 part 1 solution: counter = {counter}")
 
 
 def solution2():
-    print("Solution 2 not implemented")
+    counter = 0
+
+    with open("./day1/day1-input.txt", "r") as f:
+        num1 = f.readline()
+        num2 = f.readline()
+        num3 = f.readline()
+        num4 = f.readline()
+
+        while num4:
+            sum1 = int(num1) + int(num2) + int(num3)
+            sum2 = int(num2) + int(num3) + int(num4)
+
+            if sum2 > sum1:
+                counter = counter + 1
+
+            num1 = num2
+            num2 = num3
+            num3 = num4
+            num4 = f.readline()
+
+    print(f"Day 1 part 2 solution: counter = {counter}")
 
 
 solution1()
