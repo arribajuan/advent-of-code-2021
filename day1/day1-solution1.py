153d8f83eefdcdff9d59fdf2075f59c7409a039d
Juan Martinez <jmartinez@benchsci.com>
2021-12-06 21:16:56 -0500

    Refactor to prepare for solution 2

--- day1/day1-solution1.py
+++ day1/day1-solution1.py
@@ -1,15 +1,25 @@
-prevNum = 0
-currNum = 0
-counter = 0
 
-with open('day1/day1-input.txt', 'r')as f:
+def solution1():
+    prevNum = 0
+    currNum = 0
+    counter = 0
 
-    for line in f:
-        currNum = int(line)
+    with open('day1/day1-input.txt', 'r')as f:
 
-        if prevNum > 0 and currNum > prevNum:
-            counter = counter + 1
+        for line in f:
+            currNum = int(line)
 
-        prevNum = currNum
+            if prevNum > 0 and currNum > prevNum:
+                counter = counter + 1
 
-print(f"Counter = {counter}")
+            prevNum = currNum
+
+    print(f"Solution 1 counter = {counter}")
+
+
+def solution2():
+    print("Solution 2 not implemented")
+
+
+solution1()
+solution2()
