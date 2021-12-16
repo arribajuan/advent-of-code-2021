4ccb76d7090a69ef7f2c87fd2e3bf26b1138a467
Juan Martinez <jmartinez@benchsci.com>
2021-12-16 16:52:21 -0500

    Add day2 part2 solution

--- day2/day2-solution1.py
+++ day2/day2-solution1.py
@@ -22,7 +22,35 @@ def solution1_1():
         depth = move_down - move_up
         result = depth * move_forward
 
+    print(f"Day 2 part 1 solution: {result}")
+
+def solution2_1():
+    move_up = 0
+    move_down = 0
+    move_forward = 0
+    aim = 0
+    depth = 0
+
+    with open(input_location, "r") as f:
+        for line in f:
+            lineItems = line.split()
+            direction = lineItems[0]
+            magnitude = int(lineItems[1])
+
+            if direction == "up":
+                aim = aim - magnitude
+                #move_up = move_up + magnitude
+            if direction == "down":
+                aim = aim + magnitude
+                #move_down = move_down + magnitude
+            if direction == "forward":
+                move_forward = move_forward + magnitude
+                depth = depth + (aim * magnitude)
+
+        result = depth * move_forward
+
     print(f"Day 2 part 2 solution: {result}")
 
 
 solution1_1()
+solution2_1()
