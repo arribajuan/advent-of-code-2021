8c7027401be9f046a367e2dcc8f00a3bb4e6d7c1
Juan Martinez <jmartinez@benchsci.com>
2021-12-21 15:12:40 -0500

    Day7 part2- solution

--- day7/CrabAligner1.py
+++ day7/CrabAligner1.py
@@ -6,18 +6,15 @@ class CrabAligner1:
     cost: list[int] = []
     min_position: int = 0
     min_cost: int = 0
-    max_position: int = 0
-    max_cost: int = 0
     position_cost_cache = {0: 0}
 
     def __init__(self, input_location: str):
         self.load_input(input_location)
 
     def __str__(self):
-        result = "Crab aligner\n"
-        result += f" - total crab: {len(self.crabs)}\n"
-        result += f" - minimal cost {self.min_cost} at position {self.min_position}\n"
-        result += f" - max cost {self.max_cost} at position {self.max_position}\n"
+        result = "   Crab aligner\n"
+        result += f"   - total crab: {len(self.crabs)}\n"
+        result += f"   - minimal cost {self.min_cost} at position {self.min_position}\n"
         return result
 
     def load_input(self, input_location: str):
@@ -25,23 +22,45 @@ class CrabAligner1:
             lines = f.readlines()
         self.crabs = [int(i) for i in lines[0].strip().split(",")]
 
-    def calculate_position_costs(self):
-        typer.echo("Calculating position costs")
+    def calculate_position_costs1(self):
+        typer.echo("Calculating position costs1")
 
-        for i in range(len(self.crabs)):
-            positions_moved = 0
-            for j in range(len(self.crabs)):
-                positions_moved += abs(i - self.crabs[j])
-                cost_of_move = self.calculate_move_cost(positions_moved)
-            self.cost.append(cost_of_move)
-            typer.echo(f" - position {i}, cost {cost_of_move}")
+        with typer.progressbar(range(len(self.crabs))) as progress:
+            for i in progress:
+                positions_moved = 0
+                cost_of_move = 0
+
+                for j in range(len(self.crabs)):
+                    positions_moved += abs(i - self.crabs[j])
+                    cost_of_move = self.calculate_move_cost1(positions_moved)
+
+                self.cost.append(cost_of_move)
 
         self.min_cost = min(self.cost)
         self.min_position = self.cost.index(self.min_cost)
 
-        self.max_cost = max(self.cost)
-        self.max_position = self.cost.index(self.max_cost)
-
-    def calculate_move_cost(self, positions_moved: int):
+    def calculate_move_cost1(self, positions_moved: int):
         return positions_moved
 
+    def calculate_position_costs2(self):
+        typer.echo("Calculating position costs2")
+
+        with typer.progressbar(range(len(self.crabs))) as progress:
+            for i in progress:
+                cost_total = 0
+
+                for j in range(len(self.crabs)):
+                    positions_moved_now = abs(i - self.crabs[j])
+                    cost_now = self.calculate_move_cost2(positions_moved_now)
+                    cost_total += cost_now
+
+                self.cost.append(cost_total)
+
+        self.min_cost = min(self.cost)
+        self.min_position = self.cost.index(self.min_cost)
+
+    def calculate_move_cost2(self, positions_moved: int):
+        cost = 0
+        for i in range(positions_moved):
+            cost += i + 1
+        return cost
--- day7/day7-solution.py
+++ day7/day7-solution.py
@@ -1,18 +1,22 @@
-import CrabAligner1 as ca1
 import typer
+import CrabAligner1 as ca
 
 input_location: str = "day7-input.txt"
 
 
 def main():
-    typer.echo("Day 7 - main")
-    solution1()
+    solution()
 
 
-def solution1():
-    typer.echo("Day 7 / Part 1 - Solution 1!")
-    crab_aligner = ca1.CrabAligner1(input_location)
-    crab_aligner.calculate_position_costs()
+def solution():
+    crab_aligner = ca.CrabAligner1(input_location)
+
+    #typer.echo("Day 7 / Part 1")
+    #crab_aligner.calculate_position_costs1()
+    #print(crab_aligner)
+
+    typer.echo("Day 7 / Part 2")
+    crab_aligner.calculate_position_costs2()
     print(crab_aligner)
 
 
