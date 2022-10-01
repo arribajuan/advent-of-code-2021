f2062fb06aefd4fa9237305ff4c17459d843a4cd
arribajuan <arribajuan@gmail.com>
2022-09-30 21:39:50 -0400

    Add solution for day1 challenge 2

--- day1/dotnet-vscode/Program.cs
+++ day1/dotnet-vscode/Program.cs
@@ -23,6 +23,30 @@
             Console.WriteLine($" - Total increases: {increaseCount}");
         }
 
+        private static void Challenge2(string[] inputLines)
+        {
+            Console.WriteLine(" - Challenge 2");
+
+            int increaseCount = 0;
+            for (int i = 0; i < inputLines.Length - 4; i++)
+            {
+                int num1 = Convert.ToInt32(inputLines[i]);
+                int num2 = Convert.ToInt32(inputLines[i + 1]);
+                int num3 = Convert.ToInt32(inputLines[i + 2]);
+                int num4 = Convert.ToInt32(inputLines[i + 3]);
+
+                int sum1 = num1 + num2 + num3;
+                int sum2 = num2 + num3 + num4;
+
+                if (sum2 > sum1)
+                {
+                    increaseCount++;
+                }
+            }
+
+            Console.WriteLine($" - Total increases: {increaseCount}");
+        }
+
         static void Main(string[] args)
         {
             Console.WriteLine("Advent of Code 2021 - Day 1");
@@ -31,6 +55,7 @@
             string[] inputLines = Utilities.LoadInput(filePath);
 
             Challenge1(inputLines);
+            Challenge2(inputLines);
 
             Console.ReadKey(true);
         }
