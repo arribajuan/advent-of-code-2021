40ed11f05f58b30d03bb45a0849eb206f1b23ffd
arribajuan <arribajuan@gmail.com>
2022-10-05 18:16:38 -0400

    Initial day 2 dot net program and class library

---
+++ day2/dotnet/Day2/Day2.PositionCalculator/Utilities.cs
@@ -0,0 +1,27 @@
+﻿namespace Day2.PositionCalculator
+{
+    public class Utilities
+    {
+        public static List<int> LoadInput(string filePath)
+        {
+            List<int> result = new List<int>();
+
+            string[] fileLines = File.ReadAllLines(filePath, System.Text.Encoding.Default);
+
+            foreach (string line in fileLines)
+            {
+                Console.WriteLine(line);
+            }
+
+            //char[] spearator = { ',' };
+            //String[] strlist = fileLines[0].Split(spearator, StringSplitOptions.TrimEntries);       // Unsafe assumption that we have at least one line of texts
+
+            //foreach (String s in strlist)
+            //{
+            //    result.Add(Convert.ToInt32(s));
+            //}
+
+            return result;
+        }
+    }
+}
--- day2/dotnet/Day2/Day2/Program.cs
+++ day2/dotnet/Day2/Day2/Program.cs
@@ -1,10 +1,26 @@
-﻿namespace Day2
+﻿using Day2.PositionCalculator;
+
+namespace Day2.CLI
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+
+
+            //string filePath1 = "/Inputs/day6-input1.txt";
+            string filePath = "D:\\code\\repos\\advent-of-code-2021\\advent-of-code-2021\\day2\\dotnet\\Day2\\Day2\\Inputs\\day2-input.txt";
+
+            List<int> result = Utilities.LoadInput(filePath);
+
+            // This points to the correct path, but the files are not eing copied ... troubleshoot this
+            //string filePath1 = System.AppContext.BaseDirectory + "Inputs\\day6-input1.txt";
+
+
+
         }
+
+
     }
 }
